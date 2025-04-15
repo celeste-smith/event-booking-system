7ed58f7e0459fd93fe0cd550e6b31c35e129ea62
Celeste Smith <210046376@mycput.ac.za>
2025-04-15 14:46:15 +0200

    Update Event.php
    Event code

--- src/Event.php
+++ src/Event.php
@@ -1,1 +1,30 @@
+<?php
+
+class Event {
+    private $eventId;
+    private $title;
+    private $description;
+    private $date;
+    private $time;
+    private $location;
+    private $capacity;
+    private $status;
+
+    public function createEvent() {}
+    public function updateEvent() {}
+    public function cancelEvent() {}
+    public function isAvailable() {}
+}
+
+class Venue {
+    private $venueId;
+    private $name;
+    private $location;
+    private $capacity;
+
+    public function addVenue() {}
+    public function updateVenue() {}
+    public function removeVenue() {}
+}
+?>
 
