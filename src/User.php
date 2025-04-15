31e515c3f4458eb7875e2bc57d89a6a82f3bb91a
Celeste Smith <210046376@mycput.ac.za>
2025-04-15 13:14:56 +0200

    Update User.php
    Added my php code

--- src/User.php
+++ src/User.php
@@ -1,1 +1,58 @@
+<?php
+
+class User {
+    private $userId;
+    private $name;
+    private $email;
+    private $password;
+    private $role;
+
+    public function __construct($userId, $name, $email, $password, $role) {
+        $this->userId = $userId;
+        $this->name = $name;
+        $this->email = $email;
+        $this->password = $password;
+        $this->role = $role;
+    }
+
+    // Getter and Setter Methods
+    public function getUserId() {
+        return $this->userId;
+    }
+
+    public function getName() {
+        return $this->name;
+    }
+
+    public function getEmail() {
+        return $this->email;
+    }
+
+    public function getPassword() {
+        return $this->password;
+    }
+
+    public function getRole() {
+        return $this->role;
+    }
+
+    // Core Methods
+    public function register() {
+        // Code for user registration
+    }
+
+    public function login() {
+        // Code for user login
+    }
+
+    public function updateProfile() {
+        // Code to update user profile
+    }
+
+    public function viewBookings() {
+        // Code to view bookings
+    }
+}
+
+?>
 
