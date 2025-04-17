dfab71739ee8d0f9de8ce8cd206d5145343d37fc
Celeste Smith <210046376@mycput.ac.za>
2025-04-17 11:23:12 +0200

    Update UserFactory.php
    Adding the php code

--- creational_patterns/UserFactory.php
+++ creational_patterns/UserFactory.php
@@ -1,1 +1,33 @@
+<?php
+
+require_once __DIR__ . '/../src/User.php';
+require_once __DIR__ . '/../src/Admin.php';
+require_once __DIR__ . '/../src/Customer.php';
+
+class UserFactory
+{
+    public static function createUser(string $type, array $userData): User
+    {
+        switch (strtolower($type)) {
+            case 'admin':
+                return new Admin(
+                    $userData['id'] ?? null,
+                    $userData['name'],
+                    $userData['email'],
+                    $userData['password']
+                );
+
+            case 'customer':
+                return new Customer(
+                    $userData['id'] ?? null,
+                    $userData['name'],
+                    $userData['email'],
+                    $userData['password']
+                );
+
+            default:
+                throw new Exception("Invalid user type: " . $type);
+        }
+    }
+}
 
