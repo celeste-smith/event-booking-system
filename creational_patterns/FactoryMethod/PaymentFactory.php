1b2bd096c8d02d7b6f6073b4d9d02032cc58ee63
Celeste Smith <210046376@mycput.ac.za>
2025-04-17 11:45:55 +0200

    Update PaymentFactory.php
     (The actual Factory Method)

--- creational_patterns/FactoryMethod/PaymentFactory.php
+++ creational_patterns/FactoryMethod/PaymentFactory.php
@@ -2,18 +2,27 @@
 
 require_once 'CreditCardProcessor.php';
 require_once 'PayPalProcessor.php';
+require_once 'DebitCardProcessor.php'; // Include DebitCardProcessor
 
-class PaymentFactory
+// Factory class to decide which payment processor to use
+class PaymentProcessorFactory
 {
-    public static function createProcessor(string $type): PaymentProcessor
+    /**
+     * Factory method to return the appropriate payment processor
+     *
+     * @param string $paymentMethod
+     * @return PaymentProcessor
+     */
+    public static function createPaymentProcessor(string $paymentMethod): PaymentProcessor
     {
-        switch (strtolower($type)) {
-            case 'creditcard':
-                return new CreditCardProcessor();
-            case 'paypal':
-                return new PayPalProcessor();
-            default:
-                throw new Exception("Unsupported payment type: $type");
+        if ($paymentMethod == 'creditcard') {
+            return new CreditCardProcessor(); // Return credit card processor
+        } elseif ($paymentMethod == 'paypal') {
+            return new PayPalProcessor(); // Return PayPal processor
+        } elseif ($paymentMethod == 'debitcard') {
+            return new DebitCardProcessor(); // Return Debit Card processor
+        } else {
+            throw new Exception("Payment method {$paymentMethod} not supported.");
         }
     }
 }
