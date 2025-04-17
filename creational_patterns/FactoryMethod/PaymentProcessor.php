ded6080aa594be41f5d625acfcb5fb88ccadad89
Celeste Smith <210046376@mycput.ac.za>
2025-04-17 11:47:39 +0200

    Update PaymentProcessor.php
    updated

--- creational_patterns/FactoryMethod/PaymentProcessor.php
+++ creational_patterns/FactoryMethod/PaymentProcessor.php
@@ -1,6 +1,13 @@
 <?php
 
+// Interface for all payment processors
 interface PaymentProcessor
 {
+    /**
+     * Process a payment of a given amount
+     *
+     * @param float $amount The amount to process
+     * @return void
+     */
     public function processPayment(float $amount);
 }
