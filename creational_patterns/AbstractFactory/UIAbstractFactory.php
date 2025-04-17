1a3790d108a2e42d52bcb975a326992e0f7437ee
Celeste Smith <210046376@mycput.ac.za>
2025-04-17 12:18:25 +0200

    Update UIAbstractFactory.php
    php code

--- creational_patterns/AbstractFactory/UIAbstractFactory.php
+++ creational_patterns/AbstractFactory/UIAbstractFactory.php
@@ -1,1 +1,25 @@
+<?php
+
+require_once 'Button.php';
+require_once 'Form.php';
+
+/**
+ * Abstract Factory Interface for creating UI components
+ */
+interface UIAbstractFactory
+{
+    /**
+     * Create a Button component
+     * 
+     * @return Button
+     */
+    public function createButton(): Button;
+
+    /**
+     * Create a Form component
+     * 
+     * @return Form
+     */
+    public function createForm(): Form;
+}
 
