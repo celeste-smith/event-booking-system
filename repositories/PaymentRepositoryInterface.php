0df0fa1a52835e5a58f5674beb7a963fb69bda6a
Celeste Smith <210046376@mycput.ac.za>
2025-04-22 12:00:30 +0200

    Update PaymentRepositoryInterface.php
    php code

--- repositories/PaymentRepositoryInterface.php
+++ repositories/PaymentRepositoryInterface.php
@@ -1,1 +1,12 @@
+<?php
+// File: repositories/PaymentRepositoryInterface.php
+
+namespace EventBookingSystem\Repositories;
+
+use EventBookingSystem\Entities\Payment;
+
+interface PaymentRepositoryInterface extends RepositoryInterface
+{
+    // Define any additional methods specific to Payment entity if needed
+}
 
