267c6aa750cb2dcfa61f831a979e4dd796e19550
Celeste Smith <210046376@mycput.ac.za>
2025-04-22 11:58:00 +0200

    Update RepositoryInterface.php
    This is the php code for repository interface.

--- repositories/RepositoryInterface.php
+++ repositories/RepositoryInterface.php
@@ -1,1 +1,18 @@
+<?php
+// File: repositories/RepositoryInterface.php
+
+namespace EventBookingSystem\Repositories;
+
+interface RepositoryInterface
+{
+    public function create($entity): void;
+
+    public function read($id);
+
+    public function update($entity): void;
+
+    public function delete($entity): void;
+
+    public function getAll(): array;
+}
 
