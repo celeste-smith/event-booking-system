ed782b4ada3d3bdd763a90334801c0e1d7a7d596
Celeste Smith <210046376@mycput.ac.za>
2025-04-22 14:07:10 +0200

    Update InMemoryTicketRepositoryTest.php
    php test case for ticket repository

--- tests/InMemoryTicketRepositoryTest.php
+++ tests/InMemoryTicketRepositoryTest.php
@@ -1,1 +1,17 @@
+<?php
+// File: tests/InMemoryTicketRepositoryTest.php
+
+use PHPUnit\Framework\TestCase;
+use EventBookingSystem\Repositories\InMemory\InMemoryTicketRepository;
+use EventBookingSystem\Entities\Ticket;
+
+class InMemoryTicketRepositoryTest extends TestCase
+{
+    private $repository;
+
+    protected function setUp(): void
+    {
+        $this->repository = new InMemoryTicketRepository();
+   
+::contentReference[oaicite:0]{index=0}
 
