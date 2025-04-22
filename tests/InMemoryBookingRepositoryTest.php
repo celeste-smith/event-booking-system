31d5a7a3c1f4df7e3abe97c9b91b445bba4519c7
Celeste Smith <210046376@mycput.ac.za>
2025-04-22 14:03:02 +0200

    Update InMemoryBookingRepositoryTest.php
    php test code for Booking Repository

--- tests/InMemoryBookingRepositoryTest.php
+++ tests/InMemoryBookingRepositoryTest.php
@@ -1,1 +1,56 @@
+<?php
+// File: tests/InMemoryBookingRepositoryTest.php
+
+use PHPUnit\Framework\TestCase;
+use EventBookingSystem\Repositories\InMemory\InMemoryBookingRepository;
+use EventBookingSystem\Entities\Booking;
+
+class InMemoryBookingRepositoryTest extends TestCase
+{
+    private $repository;
+
+    protected function setUp(): void
+    {
+        $this->repository = new InMemoryBookingRepository();
+    }
+
+    public function testCreateAndRead()
+    {
+        $booking = new Booking('1', 'user1', 'event1');
+        $this->repository->create($booking);
+        $retrieved = $this->repository->read('1');
+        $this->assertSame($booking, $retrieved);
+    }
+
+    public function testUpdate()
+    {
+        $booking = new Booking('1', 'user1', 'event1');
+        $this->repository->create($booking);
+        $booking->setEventId('event2');
+        $this->repository->update($booking);
+        $retrieved = $this->repository->read('1');
+        $this->assertEquals('event2', $retrieved->getEventId());
+    }
+
+    public function testDelete()
+    {
+        $booking = new Booking('1', 'user1', 'event1');
+        $this->repository->create($booking);
+        $this->repository->delete($booking);
+        $retrieved = $this->repository->read('1');
+        $this->assertNull($retrieved);
+    }
+
+    public function testGetAll()
+    {
+        $booking1 = new Booking('1', 'user1', 'event1');
+        $booking2 = new Booking('2', 'user2', 'event2');
+        $this->repository->create($booking1);
+        $this->repository->create($booking2);
+        $allBookings = $this->repository->getAll();
+        $this->assertCount(2, $allBookings);
+        $this->assertContains($booking1, $allBookings);
+        $this->assertContains($booking2, $allBookings);
+    }
+}
 
