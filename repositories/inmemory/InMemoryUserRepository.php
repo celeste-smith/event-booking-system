8695573d926e91f10f09ced144b62b6467b4c0ba
Celeste Smith <210046376@mycput.ac.za>
2025-04-22 12:05:08 +0200

    Update InMemoryUserRepository.php
    php code

--- repositories/inmemory/InMemoryUserRepository.php
+++ repositories/inmemory/InMemoryUserRepository.php
@@ -1,1 +1,40 @@
+<?php
+// File: repositories/inmemory/InMemoryUserRepository.php
+
+namespace EventBookingSystem\Repositories\InMemory;
+
+use EventBookingSystem\Repositories\UserRepositoryInterface;
+use EventBookingSystem\Entities\User;
+
+class InMemoryUserRepository implements UserRepositoryInterface
+{
+    private array $storage = [];
+
+    public function create($entity): void
+    {
+        $this->storage[$entity->getId()] = $entity;
+    }
+
+    public function read($id)
+    {
+        return $this->storage[$id] ?? null;
+    }
+
+    public function update($entity): void
+    {
+        if (isset($this->storage[$entity->getId()])) {
+            $this->storage[$entity->getId()] = $entity;
+        }
+    }
+
+    public function delete($entity): void
+    {
+        unset($this->storage[$entity->getId()]);
+    }
+
+    public function getAll(): array
+    {
+        return array_values($this->storage);
+    }
+}
 
