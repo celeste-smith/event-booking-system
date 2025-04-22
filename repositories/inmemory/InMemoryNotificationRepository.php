bc195a37d5981198aeb24cc0f145f0107e33983a
Celeste Smith <210046376@mycput.ac.za>
2025-04-22 12:07:55 +0200

    Update InMemoryNotificationRepository.php
    php code

--- repositories/inmemory/InMemoryNotificationRepository.php
+++ repositories/inmemory/InMemoryNotificationRepository.php
@@ -1,1 +1,39 @@
+<?php
+// File: repositories/inmemory/InMemoryNotificationRepository.php
+
+namespace EventBookingSystem\Repositories\InMemory;
+
+use EventBookingSystem\Repositories\NotificationRepositoryInterface;
+use EventBookingSystem\Entities\Notification;
+
+class InMemoryNotificationRepository implements NotificationRepositoryInterface
+{
+    private array $storage = [];
+
+    public function create($entity): void
+    {
+        $this->storage[$entity->getId()] = $entity;
+    }
+
+    public function read($id)
+    {
+        return $this->storage[$id] ?? null;
+    }
+
+    public function update($entity): void
+    {
+        if (isset($this->storage[$entity->getId()])) {
+            $this->storage[$entity->getId()] = $entity;
+        }
+    }
+
+    public function delete($entity): void
+    {
+        unset($this->storage[$entity->getId()]);
+    }
+
+    public function getAll(): array
+    {
+        return array_values
+::contentReference[oaicite:0]{index=0}
 
