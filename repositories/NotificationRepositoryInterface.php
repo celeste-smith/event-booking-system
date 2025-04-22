5c41723ebfa2951f6fa97ed8ddae6c47a25074c0
Celeste Smith <210046376@mycput.ac.za>
2025-04-22 12:01:42 +0200

    Update NotificationRepositoryInterface.php
    php code

--- repositories/NotificationRepositoryInterface.php
+++ repositories/NotificationRepositoryInterface.php
@@ -1,1 +1,12 @@
+<?php
+// File: repositories/NotificationRepositoryInterface.php
+
+namespace EventBookingSystem\Repositories;
+
+use EventBookingSystem\Entities\Notification;
+
+interface NotificationRepositoryInterface extends RepositoryInterface
+{
+    // Define any additional methods specific to Notification entity if needed
+}
 
