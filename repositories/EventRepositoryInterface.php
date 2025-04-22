8431ebc275677288ae6cdc5fa628b206aceb4fbc
Celeste Smith <210046376@mycput.ac.za>
2025-04-22 11:58:38 +0200

    Update EventRepositoryInterface.php
    Php Code

--- repositories/EventRepositoryInterface.php
+++ repositories/EventRepositoryInterface.php
@@ -1,1 +1,12 @@
+<?php
+// File: repositories/EventRepositoryInterface.php
+
+namespace EventBookingSystem\Repositories;
+
+use EventBookingSystem\Entities\Event;
+
+interface EventRepositoryInterface extends RepositoryInterface
+{
+    // Define any additional methods specific to Event entity if needed
+}
 
