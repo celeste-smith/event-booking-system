8588475a5a4a536c8aaed9623646fa541c1bb6f0
Celeste Smith <210046376@mycput.ac.za>
2025-04-22 11:59:42 +0200

    Update BookingRepositoryInterface.php
    php code

--- repositories/BookingRepositoryInterface.php
+++ repositories/BookingRepositoryInterface.php
@@ -1,1 +1,13 @@
+<?php
+// File: repositories/BookingRepositoryInterface.php
+
+namespace EventBookingSystem\Repositories;
+
+use EventBookingSystem\Entities\Booking;
+
+interface BookingRepositoryInterface extends RepositoryInterface
+{
+    // Define any additional methods specific to Booking entity if needed
+}
+
 
