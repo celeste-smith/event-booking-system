619fbe50cc631905f7dd80d05f87540f025cdd7a
Celeste Smith <210046376@mycput.ac.za>
2025-04-22 11:59:08 +0200

    Update UserRepositoryInterface.php
    php code

--- repositories/UserRepositoryInterface.php
+++ repositories/UserRepositoryInterface.php
@@ -1,1 +1,12 @@
+<?php
+// File: repositories/UserRepositoryInterface.php
+
+namespace EventBookingSystem\Repositories;
+
+use EventBookingSystem\Entities\User;
+
+interface UserRepositoryInterface extends RepositoryInterface
+{
+    // Define any additional methods specific to User entity if needed
+}
 
