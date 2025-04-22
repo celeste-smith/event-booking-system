c7f5ce85d0ee0007b08476652b6d45fa572cede1
Celeste Smith <210046376@mycput.ac.za>
2025-04-22 12:06:30 +0200

    Update InMemoryPaymentRepository.php
    php code

--- repositories/inmemory/InMemoryPaymentRepository.php
+++ repositories/inmemory/InMemoryPaymentRepository.php
@@ -1,1 +1,40 @@
+<?php
+// File: repositories/inmemory/InMemoryPaymentRepository.php
+
+namespace EventBookingSystem\Repositories\InMemory;
+
+use EventBookingSystem\Repositories\PaymentRepositoryInterface;
+use EventBookingSystem\Entities\Payment;
+
+class InMemoryPaymentRepository implements PaymentRepositoryInterface
+{
+    private array $storage = [];
+
+    public function create($entity): void
+    {
+        $this->storage[$entity->getId()] = $entity;
+    }
+
+    public function read($id)
+    {
+        return $this->storage[$id] ?? null;
+    }
+
+    public function update($entity): void
+    {
+        if (isset($this->storage[$entity->getId()])) {
+            $this->storage[$entity->getId()] = $entity;
+        }
+    }
+
+    public function delete($entity): void
+    {
+        unset($this->storage[$entity->getId()]);
+    }
+
+    public function getAll(): array
+    {
+        return array_values($this->storage);
+    }
+}
 
