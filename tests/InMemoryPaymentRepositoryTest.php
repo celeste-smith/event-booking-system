f51f1ce1f7ac343ed17f058674f30e1e0475c025
Celeste Smith <210046376@mycput.ac.za>
2025-04-22 14:05:44 +0200

    Update InMemoryPaymentRepositoryTest.php
    php test case for Payment Repository

--- tests/InMemoryPaymentRepositoryTest.php
+++ tests/InMemoryPaymentRepositoryTest.php
@@ -1,1 +1,56 @@
+<?php
+// File: tests/InMemoryPaymentRepositoryTest.php
+
+use PHPUnit\Framework\TestCase;
+use EventBookingSystem\Repositories\InMemory\InMemoryPaymentRepository;
+use EventBookingSystem\Entities\Payment;
+
+class InMemoryPaymentRepositoryTest extends TestCase
+{
+    private $repository;
+
+    protected function setUp(): void
+    {
+        $this->repository = new InMemoryPaymentRepository();
+    }
+
+    public function testCreateAndRead()
+    {
+        $payment = new Payment('1', 'booking1', 100.00);
+        $this->repository->create($payment);
+        $retrieved = $this->repository->read('1');
+        $this->assertSame($payment, $retrieved);
+    }
+
+    public function testUpdate()
+    {
+        $payment = new Payment('1', 'booking1', 100.00);
+        $this->repository->create($payment);
+        $payment->setAmount(150.00);
+        $this->repository->update($payment);
+        $retrieved = $this->repository->read('1');
+        $this->assertEquals(150.00, $retrieved->getAmount());
+    }
+
+    public function testDelete()
+    {
+        $payment = new Payment('1', 'booking1', 100.00);
+        $this->repository->create($payment);
+        $this->repository->delete($payment);
+        $retrieved = $this->repository->read('1');
+        $this->assertNull($retrieved);
+    }
+
+    public function testGetAll()
+    {
+        $payment1 = new Payment('1', 'booking1', 100.00);
+        $payment2 = new Payment('2', 'booking2', 200.00);
+        $this->repository->create($payment1);
+        $this->repository->create($payment2);
+        $allPayments = $this->repository->getAll();
+        $this->assertCount(2, $allPayments);
+        $this->assertContains($payment1, $allPayments);
+        $this->assertContains($payment2, $allPayments);
+    }
+}
 
