693df293c02a5217623fca0fa45d04c13fd70008
Celeste Smith <210046376@mycput.ac.za>
2025-04-22 12:02:14 +0200

    Update TicketRepositoryInterface.php
    php code

--- repositories/TicketRepositoryInterface.php
+++ repositories/TicketRepositoryInterface.php
@@ -1,1 +1,12 @@
+<?php
+// File: repositories/TicketRepositoryInterface.php
+
+namespace EventBookingSystem\Repositories;
+
+use EventBookingSystem\Entities\Ticket;
+
+interface TicketRepositoryInterface extends RepositoryInterface
+{
+    // Define any additional methods specific to Ticket entity if needed
+}
 
