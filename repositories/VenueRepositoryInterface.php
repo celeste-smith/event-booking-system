6bc94324e475c9892373a7e6e6a0f5b47b1faaf3
Celeste Smith <210046376@mycput.ac.za>
2025-04-22 12:01:09 +0200

    Update VenueRepositoryInterface.php
    php code

--- repositories/VenueRepositoryInterface.php
+++ repositories/VenueRepositoryInterface.php
@@ -1,1 +1,12 @@
+<?php
+// File: repositories/VenueRepositoryInterface.php
+
+namespace EventBookingSystem\Repositories;
+
+use EventBookingSystem\Entities\Venue;
+
+interface VenueRepositoryInterface extends RepositoryInterface
+{
+    // Define any additional methods specific to Venue entity if needed
+}
 
