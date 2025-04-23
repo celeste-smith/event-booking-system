9b5cf7a149b8db07ad09ddf83ebaa0995f6b796d
Celeste Smith <210046376@mycput.ac.za>
2025-04-23 22:46:00 +0200

    Update NotificationRepositoryInterface.php
    Implementation of the Factory Pattern for storage abstraction.

--- repositories/NotificationRepositoryInterface.php
+++ repositories/NotificationRepositoryInterface.php
@@ -5,8 +5,10 @@ namespace EventBookingSystem\Repositories;
 
 use EventBookingSystem\Entities\Notification;
 
-interface NotificationRepositoryInterface extends RepositoryInterface
+interface NotificationRepositoryInterface
 {
-    // Define any additional methods specific to Notification entity if needed
+    public function find(int $id): ?Notification;
+    public function save(Notification $notification): void;
+    public function delete(int $id): void;
+    public function getAll(): array;
 }
-
