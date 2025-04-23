2a2e1ddda28039d6542789e7f6e168979a9ae6ac
Celeste Smith <210046376@mycput.ac.za>
2025-04-23 23:03:58 +0200

    Create RepositoryFactory.php
    php code for Repository Factory

---
+++ factories/RepositoryFactory.php
@@ -0,0 +1,23 @@
+<?php
+namespace EventBookingSystem\Factories;
+
+use EventBookingSystem\Repositories\UserRepositoryInterface;
+use EventBookingSystem\Repositories\InMemory\InMemoryUserRepository;
+// Include other repository interfaces and implementations as needed
+
+class RepositoryFactory
+{
+    public static function createUserRepository(string $storageType): UserRepositoryInterface
+    {
+        switch (strtoupper($storageType)) {
+            case 'MEMORY':
+                return new InMemoryUserRepository();
+            // case 'DATABASE':
+            //     return new DatabaseUserRepository(); // Future implementation
+            default:
+                throw new \InvalidArgumentException("Invalid storage type: $storageType");
+        }
+    }
+
+    // Add similar methods for other repositories
+}
