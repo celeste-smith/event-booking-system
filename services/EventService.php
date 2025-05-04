cea6424abbddc4ced6d9d759363b87fad9962e5b
Celeste Smith <210046376@mycput.ac.za>
2025-05-04 21:58:33 +0200

    Update EventService.php
    code for event service

--- services/EventService.php
+++ services/EventService.php
@@ -1,1 +1,37 @@
+<?php
+
+namespace Services;
+
+use Repositories\EventRepository;
+use Models\Event;
+
+class EventService
+{
+    private EventRepository $eventRepo;
+
+    public function __construct(EventRepository $eventRepo)
+    {
+        $this->eventRepo = $eventRepo;
+    }
+
+    public function createEvent(array $data): Event
+    {
+        if (empty($data['title']) || empty($data['date']) || empty($data['availableTickets'])) {
+            throw new \InvalidArgumentException("Title, date and ticket count required.");
+        }
+
+        $event = new Event($data['title'], $data['date'], (int)$data['availableTickets']);
+        return $this->eventRepo->save($event);
+    }
+
+    public function getAllEvents(): array
+    {
+        return $this->eventRepo->findAll();
+    }
+
+    public function getEventById(string $eventId): ?Event
+    {
+        return $this->eventRepo->findById($eventId);
+    }
+}
 
